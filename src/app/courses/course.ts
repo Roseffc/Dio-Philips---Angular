08a56d47fa2c3064ae8beafb605dd92d8251672b
Roseffc <roseffc@gmail.com>
2022-05-03 15:38:55 -0300

    first commit

---
+++ src/app/courses/course.ts
@@ -0,0 +1,11 @@
+export class Course {
+    id!: number | string;
+    name: any;
+    imageUrl: string | undefined;
+    price: number | undefined;
+    code: any;
+    duration: number | undefined;
+    releaseDate: string | undefined;
+    rating!: number;
+    description!: string | undefined;
+}
